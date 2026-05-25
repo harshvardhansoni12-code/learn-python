c3da14b99eb2723e7576eb67bd1cf77fa41764de
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-05-25 14:25:28 +0530

    added membership operator

--- p2.py
+++ p2.py
@@ -1,1 +1,51 @@
-print("p2")
+# Operator
+
+# Arithmetic
+print("Arithmetic")
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/6)
+print(5//3)
+print(5%2)
+print(5**2)
+
+# Relational
+print("Relational")
+print(4>5)
+print(4<5)
+print(4>=4)
+print(4<=4)
+print(4==7)
+
+# Logical (AND , OR , NOT)
+print("Logical")
+print(1 and 0)
+
+print(1 or 0)
+
+print(not 1 ,sep='\n\n')
+
+# Bitwise 
+
+# Assignment Operators
+
+print("Assignment Operators",end='\n\n')
+a = 6
+a%=5
+print(a)
+
+#  Membership operator
+print("Membership operator:" , end='\n\n\n')
+
+print('D' in 'Delhi')
+print('l' not in 'list')
+print('k' in 'delhi')
+
+
+
+
+if 'k' in 'Delhi':
+    print("i love my india")
+else:
+     print("i will still love india")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,4 @@
-# print hello world
-print("hello world")
-# take input
-# a = input("enter the name")
-print(type(True))
-#boolean is True not 'true'
-# python is case sensitive
-
-print("hello",1,0.1,True,sep='/')
-
-# data types:
+if 'k' in 'Delhi':
+    print("i love my india")
+else:
+     print("i will still love india")
