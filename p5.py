c045ba7f89b21ec4073b4d85c713687d00c93949
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-06-02 21:14:12 +0530

    done with common function

---
+++ p5.py
@@ -0,0 +1,15 @@
+# for i in 'delhi':
+#     print('pune')
+
+# print('c' in 'Delhi')
+
+s = 'delhi'
+# common functions for string
+# len
+print(len(s))
+# max
+print(max(s))
+# min
+print(min(s))
+# sorted
+print(sorted(s))
