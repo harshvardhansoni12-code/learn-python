49035eafb9a9bc53c2dc9ac41977278006aa4ab4
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-06-03 20:54:28 +0530

    added more functions of string

--- p5.py
+++ p5.py
@@ -8,8 +8,31 @@ s = 'delhi'
 # len
 print(len(s))
 # max
-print(max(s))
+print(max(s)) # max is found on the basis of ascii value of the character
 # min
 print(min(s))
 # sorted
-print(sorted(s))
+print(sorted(s)) # sorted returns a list of characters in ascending order of their ascii values
+
+# Capitalize / Title / Upper / Lower
+
+s = 'hello world'
+
+print(s.capitalize())
+
+print(s.title())
+
+print(s.upper())
+
+print(s.lower())
+
+# Count/find/index
+
+s = 'hello world'
+print(s.count('l')) # count returns the number of occurrences of the substring in the string
+print(s.find('l')) # find returns the index of the first occurrence of the substring in the string
+print(s.index('l')) # index returns the index of the first occurrence of the substring in the string
+
+# Replace
+s = 'hello world'
+print(s.replace('l', 'x')) # replace returns a new string with all occurrences of the first argument replaced by the second argument    
