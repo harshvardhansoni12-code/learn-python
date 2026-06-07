2566d0f7590b3b15b9e3c6b3523aac6fddfa2f09
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-06-07 16:45:50 +0530

    added slicing code

--- p7.py
+++ p7.py
@@ -16,3 +16,21 @@ h = [[[1,2],[2,3]],[[3,4],[4,5]]]
 print(h[0]) 
 print(h[0][0])
 print(h[0][1][1])
+
+print()
+
+l = [1, 2, 3, 4, 5]
+print(l[0:3]) # slicing list from index 0 to 2
+print(l[2:5]) # slicing list from index 2 to 4
+print(l[:3]) # slicing list from index 0 to 2
+print()
+print(l[2:]) # slicing list from index 2 to end
+print(l[:]) # slicing list from index 0 to end
+
+# NEGATIVE SLICING
+print(l[-3:]) # slicing list from index -3 to -2   
+print(l[:-2]) # slicing list from index 0 to -3
+print(l[-4:-1]) # slicing list from index -4 to -2
+print()
+print(l[0::2]) # slicing list from index 0 to end with step 2
+print(l[-5:-2:2]) # slicing list from index -5 to -3 with step 2    
